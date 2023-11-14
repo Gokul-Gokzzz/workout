4359afc4d447605ed0f483f03d7cdac35cf7b12e
Gokul-Gokzzz <gokulunniu88@gmail.com>
2023-11-14 17:58:25 +0530

    first commit

---
+++ exm.dart
@@ -0,0 +1,53 @@
+import 'dart:io';
+
+// void main(){
+//  Set<int> st = {24,143,413,5246,2456};
+//  st.contains(24);
+//  print(st);
+
+// }
+
+
+// bool ispallaindrom (String str){
+//   String pallind = str;
+//   return pallind == pallind.split('').reversed.join('');
+// }
+// void main(){
+//   stdout.write('Enter a name');
+//   String name = stdin.readLineSync()?? '';
+//   print(ispallaindrom(name));
+
+
+// String name = "dfdd";
+// int n = int.parse(name);
+// print(n);
+
+// }
+
+// List<int> findCommonElements(List<int> list1, List<int> list2) {
+//    Set<int> set1 = list1.toSet();
+//    Set<int> set2 = list2.toSet();
+//    Set<int> commonSet = set1.intersection(set2);
+//     List<int> commonList = commonSet.toList();
+//     return commonList;
+// }
+// void main(){
+//   List<int> a = [1,1,2,3,5,8,13,21,34,55,89];
+//   List<int> b = [1,2,3,4,5,6,7,8,9,10,11,12,13];
+//   List<int> commonElements = findCommonElements(a, b);
+//   print(commonElements);
+// }
+
+// String reverseorder  (String str){
+//   List<String> words = str.split('');
+//   List<String> reverse = words.reversed.toList();
+//   return reverse.join();
+// }
+// void main(){
+//   stdout.write('Enter the word');
+//   String name = stdin.readLineSync() ??'';
+//   String reversename = reverseorder(name);
+//   print(reversename); 
+// }
+
+
