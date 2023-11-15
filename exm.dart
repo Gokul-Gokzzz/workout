980c23dc96d3227d4047be48744232a3d20eed1f
Gokul-Gokzzz <gokulunniu88@gmail.com>
2023-11-15 18:15:37 +0530

    added new questions

--- exm.dart
+++ exm.dart
@@ -1,4 +1,4 @@
-import 'dart:io';
+// import 'dart:io';
 
 // void main(){
 //  Set<int> st = {24,143,413,5246,2456};
@@ -49,5 +49,134 @@ import 'dart:io';
 //   String reversename = reverseorder(name);
 //   print(reversename); 
 // }
+
+
+
+// null aware assignment operator
+// String? name;
+// void main(){
+//   name??= 'john';
+ 
+//   name??= 'manu';
+//   print(name);
+  
+
+  
+// }
+
+// void main(){
+//   List<int> numb=[1,232,45,6736,2354,782,243,34];
+//   List<int> a = [];
+//   for(int x in numb ){
+//     if(x % 1 == 0){
+//       a.add(x);
+//     }
+//   }
+//   print(a);
+
+// }
+
+// void main(){
+//   var temper = 100;
+// print('Enter the tempeture:');
+// int a= int.parse(stdin.readLineSync()!);
+//  a>temper ? print('Greater') : a<temper? print('Lower') :print('correct');
+// }
+
+
+// void main(){
+//   List<String> names = ["Alice", "Raju", "Eve", "John", "Dean", "Lia", "Olivia"];
+//   List a = [];
+//   for (var element in names) {
+//     if(element.length>3){
+//       a.add(element);
+
+//     }
+//   }
+//   a.sort();
+//   print(a);
+// }
+
+// void main(){
+//   List<int> numbersList = [2, 5, 10, 7, 14, 6, 3,-6,52,-14,11,85,-61,0,-2];
+//   int sum =0;
+//   for(int x in numbersList){
+//     if(x % 2 == 0 && x > 0){
+//       sum=sum+x;
+//     }
+    
+//   }
+// print(sum);
+// }
+
+
+// void main(){
+//     DateTime dt = DateTime.now();
+// print(dt);
+// }
+
+// void main(){
+//   List<int> a =[1,2,3,4,5,6,7,-1,-2,-5,-7];
+//   for(int x in a){
+//     if(x % 2==0 && x>0){
+//       print(x);
+//     }
+//   }
+// }
+// void main (){
+//   List<String> name =['gokul unni'];
+//   for(var x in name){
+//    var b = x.split(' ').reversed.join();
+//    print(b);
+//   }
+  
+// }
+// void main(){
+// List<double> a = [2.5, 1.5, 3.0, 4.5]; 
+// double sum =0;
+// double avg=0;
+// for(var x in a){
+//   sum+=x;
+// }
+// avg =sum/a.length;
+// print(avg);
+// }
+
+// void main(){
+//    List date = [12,5,2023];
+//     if(date.length == 3){
+//       int d=date[0];
+//       int m=date[1];
+//       int y=date[2];
+//       String a= ("${d}/${m}/${y}");
+//     print(a);
+//     }     
+    
+//    }
+
+
+
+
+
+// import 'dart:io';
+
+// void main(){
+//   Map  birthdays = { 
+//     "Albert "   : "14/03/1879",
+//     "Habeeb"    : "20/2/2015",
+//     "Benjamin " : "17/01/1706",
+//     "Irfan"     : "6/7/1985",
+//     "Franklin"  : "10/12/1815",
+// };
+// print('Enter a name');
+// String b = stdin.readLineSync()!;
+// for(var x in birthdays.keys){
+//   if(b==x){
+//     print(birthdays[x]);
+//   }
+// }
+// }
+
+
 
 
